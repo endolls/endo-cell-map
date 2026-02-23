b683cdb5643e05b164adcfefc66e1c3ef10167be
endolls <team@en-dolls.com>
2026-02-23 12:43:36 -0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Per R3F guidance for Next.js 13.1+ / latest:
+  // transpilePackages: ['three']
+  transpilePackages: ['three'],
+  reactStrictMode: true
+};
+
+module.exports = nextConfig;
